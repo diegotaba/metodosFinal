fd301fe258f8f619cf37e76d29ba78c069e2bbd1
diegotaba <dimataro@gmail.com>
2018-04-04 01:02:05 -0500

    Commit

--- src/metodos/PresentationController.java
+++ src/metodos/PresentationController.java
@@ -1,6 +1,34 @@
 package metodos;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class PresentationController {
 
+    @FXML
+    Button buttonNewtonMethod;
+
+
+    public void onMethodNewtonClickButton() throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("views/newtonMethodWindow.fxml"));
+
+        Stage newtonMethodWindow = new Stage();
+
+        newtonMethodWindow.initModality(Modality.APPLICATION_MODAL);
+        newtonMethodWindow.setTitle("Metodo de newton");
+
+        newtonMethodWindow.setScene(new Scene(root,415,460));
+        newtonMethodWindow.setResizable(false);
+        newtonMethodWindow.show();
+
+    }
 
 }
---
+++ src/metodos/newtonMethodViewController.java
@@ -0,0 +1,65 @@
+package metodos;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Created by core i 5 on 3/04/2018.
+ */
+public class newtonMethodViewController {
+
+    @FXML
+    Button back;
+
+    @FXML
+    Button clean;
+
+    @FXML
+    Button calculate;
+
+    @FXML
+    TextField firstSign;
+
+    @FXML
+    TextField secondCoeficient;
+
+    @FXML
+    TextField raizValue;
+
+    @FXML
+    TextField errorValue;
+
+    @FXML
+    TextField thirdCoficient;
+
+    @FXML
+    TextField firstCoeficient;
+
+    @FXML
+    TextField XoInitValue;
+
+    @FXML
+    TextField iterationNumber;
+
+
+    public void onClickCleanButton(){
+
+        XoInitValue.clear();
+        iterationNumber.clear();
+        errorValue.clear();
+    }
+
+    public void onClickBackButton() throws IOException {
+        Stage s = (Stage) back.getScene().getWindow();
+        s.close();
+    }
+
+
+
+
+
+}
